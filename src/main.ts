24f880cbd4083dff5390d858beb6244bcfa60e23
Inamura Tamaki <tinamura0159@gmail.com>
2023-01-24 15:03:58 +0900

    prismaService, main.tsでフロントエンドのドメイン許可を記述

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // CORS(Cross-Origin Resource Sharing) = オリジン間リソース共有
+  // バックエンドへのサービスを許可するフロントエンドのドメインを設定
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+  // フロントエンドから受け取ったcookieを解析できる
+  app.use(cookieParser());
   await app.listen(3005);
 }
 bootstrap();
