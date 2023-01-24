a042c41813f4f958de71e49f07cf247ce7c98c59
Inamura Tamaki <tinamura0159@gmail.com>
2023-01-24 12:10:58 +0900

    docker,prismaの環境設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { CartModule } from './cart/cart.module';
+import { TodoService } from './todo/todo.service';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PrismaModule, CartModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TodoService],
 })
 export class AppModule {}
