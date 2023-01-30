13f9db2a346cebab0511f99a1161900e9005e40f
Inamura Tamaki <tinamura0159@gmail.com>
2023-01-30 09:34:12 +0900

    startRental APIの作成

--- src/rental/rental.controller.ts
+++ src/rental/rental.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+} from '@nestjs/common';
+import { UpdateRentalHistoryDto } from './dto/update-rental.dto';
 import { RentalService } from './rental.service';
 
 @Controller('rental')
@@ -8,5 +16,13 @@ export class RentalController {
   @Get(':id')
   selectRental(@Param('id', ParseIntPipe) id: number) {
     return this.rentalService.selectRental(id);
+  }
+
+  @Patch('update/:id')
+  updateRentalHistory(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateRentalHistoryDto,
+  ) {
+    return this.rentalService.updateRentalHistory(id, dto);
   }
 }
